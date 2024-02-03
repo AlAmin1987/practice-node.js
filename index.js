176e96e0ab27a3b341e8b1bbae40cebec699d3bf
AlAmin Islam <alamin.dreams2021@gmail.com>
2024-02-03 13:00:41 +0600

    demo text create

--- index.js
+++ index.js
@@ -1,3 +1,36 @@
 
-// Http Server Create
+let fs=require("fs")
+let http = require("http")
+
+let server = http.createServer(function(req,res){
+
+  if(req.url=="/about"){
+
+    fs.writeFile("demo.text","Hello World!",function(error){
+       
+       
+       if(error){
+        res.writeHead(200,{"Content-Type":"text/html"})
+        res.write("Demo text Added Fail")
+        res.end()
+       }
+       else{ 
+        res.writeHead(200,{"Content-Type":"text/html"})
+       res.write("Demo Text Create")
+       res.end()
+
+       }
+      
+
+    })
+  }
+  
+
+
+})
+
+server.listen(5500)
+console.log("Server run success");
+
+
 
