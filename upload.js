ced4eef287b53287dd9d17c16c9ad4650ff442d4
AlAmin Islam <alamin.dreams2021@gmail.com>
2024-02-06 22:46:11 +0600

    upload success

--- upload.js
+++ upload.js
@@ -2,9 +2,26 @@ const express = require("express")
 const multer = require('multer')
 const app = express()
 
+// File Upload
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/uploads')
+    },
+    filename: function (req, file, cb) {
+      const name = Date.now() + '-' + file.originalname
+      cb(null, name)
+    }
+  })
+  
+  const upload = multer({ storage: storage })
+
 app.get("/submitted", function(req,res){
-    res.status(200).send(__dirname +"/index.html")
+    res.status(200).sendFile(__dirname +"/index.html")
 })
+app.post("/submitted",upload.single("images"), function(req,res){
+    res.status(200).send("File uploaded")
+})
+
 app.get('/test',function(req,res){
     res.status(200).send("Api Running")
 })
